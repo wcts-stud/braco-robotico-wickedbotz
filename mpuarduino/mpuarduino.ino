ed085f12774485464f6abacf807cd7d0c8ff457a
Willyam Castro <willyam.castro@catolicasc.org.br>
2019-01-26 15:58:30 -0200

    #1 alteração de calibragem - desnecessário

--- mpuarduino/mpuarduino.ino
+++ mpuarduino/mpuarduino.ino
@@ -25,7 +25,7 @@ const int iLED = 13;
 
 int16_t GAxes[6] = {0, 0, 0, 0, 0, 0};
 
-
+void calibrar();
 
 
 // This function read Nbytes bytes from I2C device at address Address. 
@@ -80,7 +80,7 @@ void setup()
   I2CwriteByte(MPU9250_ADDRESS,28,ACC_FULL_SCALE_4_G);
 
   
-   pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT);
   Timer1.initialize(10000);         // initialize timer1, and set a 1/2 second period
   Timer1.attachInterrupt(callback);  // attaches callback() as a timer overflow interrupt
   
@@ -107,11 +107,12 @@ void loop()
 {
   while (!intFlag);
   intFlag=false;
-  
+
+  /*
   // Display time
   Serial.print (millis()-ti,DEC);
   Serial.print ("\t");
-
+  */
   
   // _______________
   // ::: Counter :::
@@ -131,7 +132,7 @@ void loop()
   
   // Create 16 bits values from 8 bits data
 
-/*  
+  /*  
   // Accelerometer
   int16_t ax=-(Buf[0]<<8 | Buf[1]);
   int16_t ay=-(Buf[2]<<8 | Buf[3]);
@@ -142,7 +143,7 @@ void loop()
   int16_t gx = -(Buf[8]<<8 | Buf[9]);
   int16_t gy = -(Buf[10]<<8 | Buf[11]);
   int16_t gz = Buf[12]<<8 | Buf[13];
-*/
+  */
   
   int16_t gx;
   int16_t gy;
@@ -150,38 +151,45 @@ void loop()
 
 
   if (GAxes[0] == 0) {
+    
     gx = -(Buf[8]<<8 | Buf[9]);
     gy = -(Buf[10]<<8 | Buf[11]);
     gz = Buf[12]<<8 | Buf[13];
 
-  
+    // TODO: Mostra valor da leitura;
     Serial.print(gx);
     Serial.print(", ");
     Serial.print(gy);
     Serial.print(", ");
-    Serial.println(gz);
+    Serial.print(gz);
+    Serial.println("** ");
 
-    
   } else {
+    
     GAxes[3] = -(Buf[8]<<8 | Buf[9]);
-    gx = (-(Buf[8]<<8 | Buf[9])) - GAxes[0];
+    //gx = (-(Buf[8]<<8 | Buf[9])) - GAxes[0]; // ou gx -= GAxes[0];
+    gx = GAxes[3] - GAxes[0]; 
     GAxes[4] = -(Buf[10]<<8 | Buf[11]);
-    gy = (-(Buf[10]<<8 | Buf[11])) - GAxes[1];
+    //gy = (-(Buf[10]<<8 | Buf[11])) - GAxes[1];  // ou gy -= GAxes[1];
+    gy = GAxes[4] - GAxes[1];
     GAxes[5] = Buf[12]<<8 | Buf[13];
-    gz = (Buf[12]<<8 | Buf[13]) - GAxes[2];
+    //gz = (Buf[12]<<8 | Buf[13]) - GAxes[2]; // ou gz -= GAxes[2];
+    gz = GAxes[5] - GAxes[2];
 
-//valor original e calibrado...
+    // TODO: Mostra valor da leitura ( ** ) e calibrado ( cc )...
     Serial.print(GAxes[3]);
-    Serial.print(", ");
-    Serial.print(gx);
     Serial.print(", ");
     Serial.print(GAxes[4]);
     Serial.print(", ");
+    Serial.print(GAxes[5]);
+    Serial.print("** \t \t \t");
+    Serial.print(gx);
+    Serial.print(", ");
     Serial.print(gy);
     Serial.print(", ");
-    Serial.print(GAxes[5]);
-    Serial.print(", ");
-    Serial.println(gz);
+    Serial.print(gz);
+    Serial.println(" cc ");
+    
   }
 
 
@@ -197,8 +205,7 @@ void loop()
   Serial.print ("Ay\t");
   Serial.print (az,DEC);  
   Serial.print ("Az\t");
-  */
-  
+
   // Gyroscope
   Serial.print (gx,DEC); 
   Serial.print ("Gx\t");
@@ -206,7 +213,7 @@ void loop()
   Serial.print ("Gy\t");
   Serial.print (gz,DEC);  
   Serial.print ("Gz\t");
-
+  */
 
 
 
@@ -219,11 +226,11 @@ void loop()
 
         Serial.println("Calibrando...");
 
-        GAxes[0] = -(Buf[8]<<8 | Buf[9]);
-        GAxes[1] = -(Buf[10]<<8 | Buf[11]);
-        GAxes[2] = (Buf[12]<<8 | Buf[13]);
+       GAxes[0] = -(Buf[8]<<8 | Buf[9]);
+       GAxes[1] = -(Buf[10]<<8 | Buf[11]);
+       GAxes[2] = (Buf[12]<<8 | Buf[13]);
 
-/*
+        /*
         // Trata de valores negativos;
         for (int i=0; i < 3; i++) {
           if ( GAxes[i] < 0 ) {
@@ -234,8 +241,7 @@ void loop()
             //Serial.println(GAxes[i]);
           }
         }
-*/
-
+        */
         
         Serial.print("\nGX: ");
         Serial.print(GAxes[0]);
@@ -244,34 +250,26 @@ void loop()
         Serial.print("\t\t GZ: ");
         Serial.print(GAxes[2]);
         Serial.println(" - Valor de calibragem");  
-
         
 
         // TODO: LED não altera valor      
         digitalWrite(iLED, !digitalRead(iLED));
-        break;
-
+        break;  
         
     }
   }
 
-
+/*
+// TODO: Confirmar alteração do valor fora do escopo;
 Serial.print("GY = ");
 Serial.println(GAxes[1]);
-
+*/
   
   
   
   // End of line
   Serial.println("");
-//  delay(100);    
+  delay(100);    
 }
-
-
-
-
-
-
-
 
 
